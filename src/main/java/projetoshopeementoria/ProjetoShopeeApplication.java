6e60c42f2b734814d20b082b7f0c9dc6adbbd4b8
Brunobiuu <bruno17henrique@gmail.com>
2024-04-02 11:49:39 -0300

    Commit inicial projeto shopee

---
+++ src/main/java/projetoshopeementoria/ProjetoShopeeApplication.java
@@ -0,0 +1,13 @@
+package projetoshopeementoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoShopeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoShopeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetoshopeementoria/ProjetoShopeeApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoshopeementoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoShopeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
