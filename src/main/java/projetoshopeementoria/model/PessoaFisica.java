6db6f10d3bd868c6ebdedc7a8fcdcbf1af08f463
Brunobiuu <161388168+Brunobiu@users.noreply.github.com>
2024-04-03 22:29:29 -0300

    Adicionei todas as classe e enums

---
+++ src/main/java/projetoshopeementoria/model/PessoaFisica.java
@@ -0,0 +1,47 @@
+package projetoshopeementoria.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaFisica extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(nullable = false)
+	private String cpf;
+	
+	@Temporal(TemporalType.DATE)
+	private Date datanascimento;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDatanascimento() {
+		return datanascimento;
+	}
+
+	public void setDatanascimento(Date datanascimento) {
+		this.datanascimento = datanascimento;
+	}
+	
+	
+	
+	
+	
+	
+
+}
