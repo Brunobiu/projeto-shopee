bf08438f1a088397dffff560852e6068134a3507
Brunobiuu <161388168+Brunobiu@users.noreply.github.com>
2024-04-07 12:56:24 -0300

    Anotar a classe application do Spring boot

--- src/main/java/projetoshopeementoria/ProjetoShopeeApplication.java
+++ src/main/java/projetoshopeementoria/ProjetoShopeeApplication.java
@@ -2,8 +2,16 @@ package projetoshopeementoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "projetoshopeementoria.model")
+@ComponentScan(basePackages = {"projetoshopeementoria.*"})
+@EnableJpaRepositories(basePackages = {"projetoshopeementoria.repository"})
+@EnableTransactionManagement
 public class ProjetoShopeeApplication {
 
 	public static void main(String[] args) {
